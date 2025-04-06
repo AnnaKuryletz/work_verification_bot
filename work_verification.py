c43cd614a0f1162f8600b03cfb2de49a33f5470c
anyukurilets <anyu.kutilez@mail.ru>
2025-04-06 16:04:37 +0300

    delay of the next connection attempt

--- work_verification.py
+++ work_verification.py
@@ -1,12 +1,15 @@
 import requests
 import asyncio
+import os
+from dotenv import load_dotenv
 
 
 async def work_verification():
     url = "https://dvmn.org/api/long_polling/"
-    headers = {"Authorization": "Token 0a825180eb5de7f543503fa7ead0985579f2c83d"}
+    token = os.getenv("DVMN_TOKEN")
+    headers = {"Authorization": f"Token {token}"}
     params = {}
-
+    connection_error_delay = 5
     while True:
         try:
             response = await asyncio.to_thread(
@@ -14,22 +17,24 @@ async def work_verification():
             )
             response.raise_for_status()
             verification_report = response.json()
-
+            connection_error_delay = 5
             if (
                 "new_attempts" in verification_report
                 and verification_report["new_attempts"]
             ):
 
-                return verification_report[
-                    "new_attempts"
-                ]  
+                return verification_report["new_attempts"]
 
             if "last_attempt_timestamp" in verification_report:
                 params["timestamp"] = verification_report["last_attempt_timestamp"]
 
         except requests.exceptions.ReadTimeout:
-            print("Сервер не ответил за 90 секунд. Отправляю новый запрос...")
+            pass
         except requests.exceptions.ConnectionError:
-            print("Нет соединения с интернетом")
+            print(
+                f"Нет соединения с интернетом. Повтор через {connection_error_delay} сек."
+            )
+            await asyncio.sleep(connection_error_delay)
+            connection_error_delay = min(connection_error_delay * 2, 300)
 
         await asyncio.sleep(2)
