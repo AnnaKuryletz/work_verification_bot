bded02e2963185795692bda3904c02196d9859bb
anyukurilets <anyu.kutilez@mail.ru>
2025-04-06 16:20:32 +0300

    the correct letter case is used

--- bot.py
+++ bot.py
@@ -1,23 +1,22 @@
 import os
-import asyncio
 from telegram import Bot
 from dotenv import load_dotenv
-from work_verification import get_review_results
+from work_verification import fetch_review_attempts
 
 
 
-async def main():
+def main():
     load_dotenv()
-    TOKEN = os.getenv("BOT_TOKEN")
-    CHAT_ID = os.getenv("CHAT_ID")
-    if not TOKEN:
+    bot_token = os.getenv("BOT_TOKEN")
+    chat_id = os.getenv("CHAT_ID")
+    if not bot_token:
         raise ValueError("BOT_TOKEN не найден в .env файле!")
-    if not CHAT_ID:
+    if not bot_token:
         raise ValueError("CHAT_ID не найден в .env файле!")
 
-    bot = Bot(token=TOKEN)
+    bot = Bot(token=bot_token)
     while True:
-        new_attempts = await get_review_results()
+        new_attempts = fetch_review_attempts()
         if isinstance(new_attempts, list):
             for attempts in new_attempts:
                 lesson_title = attempts.get("lesson_title", "Неизвестный урок")
@@ -25,11 +24,12 @@ async def main():
                 is_negative = attempts.get("is_negative", True)
                 if is_negative:
                     negative_message = f"У вас проверили работу: '{lesson_title}'!\n\nК сожалению нашлись ошибки.\nСсылка на работу {lesson_url}"
-                    await bot.send_message(chat_id=CHAT_ID, text=negative_message)
+                    bot.send_message(chat_id=chat_id, text=negative_message)
                 else:
                     positive_message = f"У вас проверили работу: '{lesson_title}'!\n\nПреподавателю всё понравилось, можно приступать к следующему уроку!\nСсылка на работу {lesson_url}"
-                    await bot.send_message(chat_id=CHAT_ID, text=positive_message)
+                    bot.send_message(chat_id=chat_id, text=positive_message)
+                
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
--- work_verification.py
+++ work_verification.py
@@ -1,40 +1,38 @@
 import requests
-import asyncio
+import time
 import os
 from dotenv import load_dotenv
 
+load_dotenv()
 
-async def get_review_results():
+def fetch_review_attempts():
     url = "https://dvmn.org/api/long_polling/"
-    token = os.getenv("DVMN_TOKEN")
-    headers = {"Authorization": f"Token {token}"}
+    dvmn_token = os.getenv("DVMN_TOKEN")
+
+    headers = {"Authorization": f"Token {dvmn_token}"}
     params = {}
-    connection_error_delay = 5
+    connection_error_delay = 5  
+
     while True:
         try:
-            response = await asyncio.to_thread(
-                requests.get, url, headers=headers, params=params, timeout=90
-            )
+            response = requests.get(url, headers=headers, params=params, timeout=90)
             response.raise_for_status()
             verification_report = response.json()
-            connection_error_delay = 5
-            if (
-                "new_attempts" in verification_report
-                and verification_report["new_attempts"]
-            ):
 
+            connection_error_delay = 5  
+
+            if "new_attempts" in verification_report and verification_report["new_attempts"]:
                 return verification_report["new_attempts"]
 
             if "last_attempt_timestamp" in verification_report:
                 params["timestamp"] = verification_report["last_attempt_timestamp"]
 
         except requests.exceptions.ReadTimeout:
-            pass
+            pass 
+
         except requests.exceptions.ConnectionError:
-            print(
-                f"Нет соединения с интернетом. Повтор через {connection_error_delay} сек."
-            )
-            await asyncio.sleep(connection_error_delay)
+            print(f"Нет соединения с интернетом. Повтор через {connection_error_delay} сек.")
+            time.sleep(connection_error_delay)
             connection_error_delay = min(connection_error_delay * 2, 300)
 
-        await asyncio.sleep(2)
+        time.sleep(2)
